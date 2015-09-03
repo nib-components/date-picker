34b1140e0ca4880f35592bceefdad91aee11971a
Ethan Williams <E.Williams@nib.com.au>
2015-09-03 11:18:47 +1000

    remove dependency on backbone/jquery/underscore

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 var Calendar = require('calendar');
-var Popover = require('popover');
-var moment = require('moment');
+var Popover  = require('popover');
+var moment   = require('moment');
+var Emitter  = require('emitter');
+var delegate = require('delegate');
 
 var DatePicker = function(options){
-  _.bindAll(this, 'hide', 'show', '_onKeyUp', '_onClick');
+    
+  this.hide = this.hide.bind(this);
 
-  var el = this.el = $(options.el);
+  var el = this.el = options.el;
 
   // Whenever the element is clicked we shwo the picker
-  el.on('click', this._onClick);
+  el.addEventListener('click', this._onClick.bind(this));
 
   // A calendar view will be rendered inside of a popover
   this.calendar = new Calendar();
 
   // The returned date format
-  this.calendar.format = options.format || el.attr('data-format') || 'DD/MM/YYYY';
+  this.calendar.format = options.format || el.getAttribute('data-format') || 'DD/MM/YYYY';
 
   // The calendar is placed in a popover that is attached
   // to the element we've passed in
@@ -26,24 +29,25 @@ var DatePicker = function(options){
 
   // When a date is selected in the calendar we'll
   // format the date and emit our own event.
-  this.calendar.on('select', this._onDateSelect, this);
+  this.calendar.on('select', this._onDateSelect.bind(this), this);
 
   // Automatically assign the date to an input if there
   // is a data attribute we can use or if the attached field
   // is a text input we can just use this el.
-  if( el.is('input[type="text"]') ) {
+  var target = el.getAttribute('data-target');
+  if( el.nodeName === 'input' && el.getAttribute('type') === 'text') {
     this._input = el;
   }
-  else if( el.attr('data-target') ) {
-    this._input = $(el.attr('data-target'));
+  else if( target ) {
+    this._input = document.querySelector(target);
   }
 
   if( this._input ) {
-    this._input.on('keyup', this._onKeyUp);
+    this._input.addEventListener('keyup', this._onKeyUp.bind(this));
   }
 };
 
-_.extend(DatePicker.prototype, Backbone.Events, {
+DatePicker.prototype = {
 
   // Hidden by default
   isVisible: false,
@@ -52,20 +56,22 @@ _.extend(DatePicker.prototype, Backbone.Events, {
   hide: function(){
     this.popover.hide();
     this.isVisible = false;
-    $(document).off('click', this.hide);
-    this.calendar.el.off('.picker');
+    document.removeEventListener('click', this.hide);
+    delegate.unbind(this.calendar.el, 'click', this._onPickerClick, false);
+  },
+  
+  _onPickerClick: function(event){
+     event.stopPropagation();
   },
 
   // Show the date picker
   show: function(){
-    $(document).on('click', this.hide);
+    document.addEventListener('click', this.hide);
 
     // When clicking on the calendar itself, we don't want
     // to close it, we so stop the event from bubbling up. This
     // lets the calendar close when clicking outside of the element
-    this.calendar.el.on('click.picker', function(event){
-      event.stopPropagation();
-    });
+    delegate.bind(this.calendar.el, '.picker', 'click', this._onPickerClick);
 
     // Show the calendar, obviously.
     this.popover.show();
@@ -78,12 +84,13 @@ _.extend(DatePicker.prototype, Backbone.Events, {
 
     // Set the date in a text field if one has been assigned
     if( this._input ) {
-      this._input.val(str).trigger('change');
+      this._input.value = str;
+      this.emit('change', str);
     }
 
     // Trigger an event so anything watching
     // this view can do something with the date selection
-    this.trigger('select', str, date);
+    this.emit('select', str, date);
   },
 
   // When the element is clicked, this could be a text input
@@ -93,7 +100,7 @@ _.extend(DatePicker.prototype, Backbone.Events, {
     event.stopPropagation();
 
     // if theres entered value in the textfield, call _setValue()
-    if(this._input.val()){
+    if(this._input.value){
       this._setValue();
     }
 
@@ -108,11 +115,12 @@ _.extend(DatePicker.prototype, Backbone.Events, {
 
   // Pass the value through to the calendar to select the specific date
   _setValue: function(){
-    var fieldValue = moment(this._input.val(), "DD/MM/YYYY");
+    var fieldValue = moment(this._input.value, "DD/MM/YYYY");
     if (fieldValue.isValid()) {
       this.calendar.select(fieldValue);
     }
   }
 });
 
+Emitter(DatePicker.prototype);
 module.exports = DatePicker;
